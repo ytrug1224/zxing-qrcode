fb2c959abf2a0dbc300e7c4117c6fe42f107d41b
zouxinjie <zouxinjie110@126.com>
2019-11-07 23:02:58 +0800

    【M】升级到Androidx

--- app/src/main/java/com/uuch/android_zxinglibrary/BaseActivity.java
+++ app/src/main/java/com/uuch/android_zxinglibrary/BaseActivity.java
@@ -1,7 +1,7 @@
 package com.uuch.android_zxinglibrary;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
--- lib-zxing/src/main/java/com/uzxing/lib/activity/CaptureActivity.java
+++ lib-zxing/src/main/java/com/uzxing/lib/activity/CaptureActivity.java
@@ -3,7 +3,7 @@ package com.uzxing.lib.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 import com.uzxing.lib.R;
